a953caa20f8259a2c1342c88080a105bc69077ea
Raymon Marsman <rmarsman@conclusion.nl>
2019-08-26 15:35:48 +0200

    simpliefied syntax

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,11 +4,14 @@ import * as React from 'react';
 export interface ITitleProps {
   content: string;
 }
+// Old school ways be like:
+// class Title extends React.Component<ITitleProps> {
+//   public render() {
+//     return <h1>{this.props.content}</h1>;
+//   }
+//  }
 
-class Title extends React.Component<ITitleProps> {
-  public render() {
-    return <h1>{this.props.content}</h1>;
-  }
-}
+// Legit school ways be like:
+const Title = ({ content }: ITitleProps) => <h1>{content}</h1>;
 
 export default Title;
