fea6686f7267389a0e1588c7980cd8ef496d181b
Raymon Marsman <rmarsman@conclusion.nl>
2019-08-27 10:45:15 +0200

    first attempt at adding a toggle state button

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -4,14 +4,28 @@ export interface IUserProps {
   name: string;
 }
 
-export default class User extends React.Component<IUserProps> {
+export interface IUserState {
+  present: boolean;
+}
+
+export default class User extends React.Component<IUserProps, IUserState> {
+  public state = { present: false };
+
   public render() {
     return (
       <div>
         <p>
           User:<b> {this.props.name}</b>
+          <p>
+            {this.props.name} is <b>{this.state.present ? 'not' : false}</b> present
+          </p>
+          <button onClick={this.handleClick}>Toggle</button>
         </p>
       </div>
     );
   }
+
+  private handleClick = () => {
+    this.setState({ present: !this.state.present });
+  };
 }
